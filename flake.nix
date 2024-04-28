e1ad2c3b784662919dc10ff6166d0328a67024fd
Benjamin Cape <benjcape@gmail.com>
2024-04-28 16:00:06 +0000

    python example

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     fenix.url = "github:nix-community/fenix";
@@ -8,16 +7,15 @@
     naersk.url = "github:nix-community/naersk";
   };
 
-  outputs =
-    { self
-    , flake-utils
-    , naersk
-    , fenix
-    , nixpkgs
-    }:
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    fenix,
+    nixpkgs,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
+      system: let
         pkgs = (import nixpkgs) {
           inherit system;
           overlays = [
@@ -30,7 +28,8 @@
         toolchain =
           (fenix.packages.${system}.toolchainOf {
             sha256 = "sha256-IUZ+84Dg0ubNlH8jwG0hRa4F9Ye4uRYBNzC90+eqalc=";
-          }).toolchain;
+          })
+          .toolchain;
 
         naersk' = pkgs.callPackage naersk {
           cargo = toolchain;
@@ -46,31 +45,64 @@
         };
 
         shell = pkgs.mkShell {
-          inputsFrom = [ crops ];
+          inputsFrom = [crops];
 
-          nativeBuildInputs = with pkgs; [ ctypesgen rust-cbindgen ]; 
+          nativeBuildInputs = with pkgs; [ctypesgen rust-cbindgen simple.python];
         };
 
-        mkExample = { name }: naersk'.buildPackage {
-          inherit name;
-          src = ./examples + "/${name}";
-          copyLibs = true;
-          nativeBuildInputs = with pkgs; [ cargo-expand rust-cbindgen ];
-          preBuild = ''
-            ln -s ${./crops} crops
-          '';
-          postInstall = ''
-            mkdir $out/include
-            cbindgen --config $src/cbindgen.toml --crate ${name} --output $out/include/${name}.h
-          '';
-        };
+        mkExample = {name}: let
+          lib = naersk'.buildPackage {
+            inherit name;
+            src = ./examples + "/${name}";
+            copyLibs = true;
+            nativeBuildInputs = with pkgs; [cargo-expand rust-cbindgen];
+            preBuild = ''
+              ln -s ${./crops} crops
+            '';
+            postInstall = ''
+              cbindgen --config $src/cbindgen.toml --crate ${name} --output $out/include/${name}.h
+            '';
+          };
+
+          python = let
+            _pyproject = pkgs.writeTextFile {
+              name = "${name}-pyproject.toml";
+              text = ''
+                [project]
+                name = "${name}"
+                version = "0.1.0"
+              '';
+            };
+
+            _src = pkgs.runCommand "${name}-py-src" {} ''
+              mkdir $out
+
+              ln -s ${_pyproject} $out/pyproject.toml
+            '';
+          in
+            pkgs.python3Packages.buildPythonPackage {
+              inherit name;
+              src = _src;
+
+              format = "pyproject";
+
+              nativeBuildInputs = with pkgs.python3Packages; [
+                pkgs.ctypesgen
+              ];
+
+              propagatedBuildInputs = [lib];
+
+              preBuild = ''
+                mkdir -p src/${name}
+                ctypesgen -L${lib}/lib -l${name} ${lib}/include/*.h -o src/${name}/__init__.py
+              '';
+            };
+        in {inherit lib python;};
 
         simple = mkExample {
           name = "simple";
         };
-
-      in
-      rec {
+      in rec {
         # For `nix build` & `nix run`:
         packages = {
           default = crops;
